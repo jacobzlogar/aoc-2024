ecfec3146afa042b5edfd113680eca0b865c35ad
jacob zlogar <jacob@vermill.io>
2024-12-11 15:17:38 -0500

    try using binary heaps

--- src/day9.rs
+++ src/day9.rs
@@ -1,4 +1,5 @@
 use std::collections::BinaryHeap;
+use std::cmp::{Ord, Ordering, Reverse};
 
 pub fn run() -> miette::Result<()> {
     let data = crate::get_input("day9")?;
@@ -11,83 +12,105 @@ fn part_1(data: &str) -> usize {
     fs.solve(false)
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd)]
 struct File {
     id: usize,
     file_size: usize,
     block_size: isize 
 }
 
+impl Ord for File {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.block_size.cmp(&other.block_size)
+    }
+}
+
 #[derive(Debug, Clone)]
 struct FileSystem {
+    heap: BinaryHeap<File>,
     files: Vec<File>
 }
 
 impl FileSystem {
-    fn remaining_blocksize(&mut self) -> usize {
-        self.files.iter().fold(0, |acc, e| acc + e.block_size as usize)
-    }
+    // fn remaining_blocksize(&mut self) -> usize {
+    //     self.files.iter().fold(0, |acc, e| acc + e.block_size as usize)
+    // }
 
     #[tracing::instrument(skip_all)]
     fn solve(&mut self, contiguous: bool) -> usize {
-        let mut output: Vec<usize> = vec![];
-        let mut file_map = self.build();
-        let mut files = self.files.clone();
-        for i in 0..files.len() {
-            let mut file = files[i];
-            if file.file_size == 0 {
-                break;
-            }
-            output.extend(std::iter::repeat(file.id).take(file.file_size).collect::<Vec<usize>>());
-            if let Some(last) = file_map.pop() {
-                if let Some(existing) = files.iter_mut().find(|f| f.id == last) {
-                    existing.file_size = 0;
-                    existing.block_size = 0;
-                }
-                output.push(last);
-            }
-            file.block_size = 0;
-        }
-        output.iter().enumerate().fold(0, |acc, (id, value)| acc + (id * value))
+        println!("{:?}", self.files);
+        // let mut output: Vec<usize> = vec![];
+        // let mut file_map = self.build();
+        // let mut files = self.files.clone();
+        // for i in 0..files.len() {
+        //     let mut file = files[i];
+        //     if file.file_size == 0 {
+        //         break;
+        //     }
+        //     output.extend(std::iter::repeat(file.id).take(file.file_size).collect::<Vec<usize>>());
+        //     if let Some(last) = file_map.pop() {
+        //         if let Some(existing) = files.iter_mut().find(|f| f.id == last) {
+        //             existing.file_size = 0;
+        //             existing.block_size = 0;
+        //         }
+        //         output.push(last);
+        //     }
+        //     file.block_size = 0;
+        // }
+        // output.iter().enumerate().fold(0, |acc, (id, value)| acc + (id * value))
+        1
     }
 
     fn print_files(&mut self) {
-        for file in self.files.clone() {
-            for _ in 0..file.file_size {
-                print!("{}", file.id)
-            }
-            for _ in 0..file.block_size {
-                print!(".")
-            }
-        }
+        // for file in self.files.clone() {
+        //     for _ in 0..file.file_size {
+        //         print!("{}", file.id)
+        //     }
+        //     for _ in 0..file.block_size {
+        //         print!(".")
+        //     }
+        // }
         println!("")
     }
 
     fn build(&mut self) -> Vec<usize> {
-        self.files.iter().flat_map(|file| {
-            return std::iter::repeat(file.id).take(file.file_size);
-        }).collect::<Vec<usize>>()
+        // self.files.iter().flat_map(|file| {
+        //     return std::iter::repeat(file.id).take(file.file_size);
+        // }).collect::<Vec<usize>>()
+        vec![]
     }
     
     #[tracing::instrument(skip_all)]
     fn parse(text: &str) -> Self {
-        let mut files: Vec<File> = vec![];
+        let mut heap = BinaryHeap::new();
+        let mut files = vec![];
+        let mut char_iter = text.chars().enumerate().peekable();
         let mut id = 0;
-        text.chars().enumerate().for_each(|(index, ch)| {
-            if let Some(digit) = ch.to_digit(10) {
-                if index % 2 == 0 {
-                    files.push(File {
+        while let Some((_, file_size)) = char_iter.next() {
+            let file_size = file_size.to_digit(10).unwrap() as usize; 
+            if let Some((_, block_size)) = char_iter.next() {
+                if let Some(block_size) = block_size.to_digit(10) {
+                    let file = File {
                         id,
-                        file_size: digit as usize,
-                        block_size: 0
-                    });
+                        block_size: block_size as isize,
+                        file_size
+                    };
                     id += 1;
-                } else {
-                    files[id-1].block_size = digit as isize;
+                    heap.push(file);
+                    files.push(file)
+                        
                 }
+            } else {
+                let file = File {
+                    id,
+                    file_size,
+                    block_size: 0,
+                };
+                heap.push(file);
+                files.push(file)
             }
-        });
-        Self { files }
+        }
+        Self { files, heap }
     }
 }
 
@@ -96,5 +119,6 @@ const TEST: &str = "2333133121414131402";
 #[cfg(test)]
 #[test]
 fn test_day9_part1() {
-    assert_eq!(part_1(TEST), 1928);
+    println!("{:?}", part_1(TEST));
+    //assert_eq!(part_1(TEST), 1928);
 }
